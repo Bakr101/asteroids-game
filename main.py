c0cf195b19ea60137413d643d25ea091b645eddf
Bakr101 <ahmed98bakr@gmail.com>
2024-10-08 15:57:29 +0300

    initalizing the project

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import * 
+
+def main():
+    pygame.init()
+    print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}\n")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("#000000", rect=None, special_flags=0)
+        pygame.display.flip
+
+
+if __name__ == '__main__':
+    main()
