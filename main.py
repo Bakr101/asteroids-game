3b7fff5822e9050c7783b8c554760f7436f2ed89
Bakr101 <ahmed98bakr@gmail.com>
2024-10-08 16:19:59 +0300

    added clock and dt

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from constants import *
 
 def main():
     pygame.init()
-    print(f"Starting asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}\n")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("#000000", rect=None, special_flags=0)
-        pygame.display.flip
+            
+        screen.fill("#000000")
+        pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == '__main__':
